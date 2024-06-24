ad047e448aa4ea3c262998f238d60210fccdeaff
Horleryheancarh <oladipoolayinka7@gmail.com>
2024-06-24 07:12:06 +0100

    finished home page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,27 @@
+import { motion } from 'framer-motion';
+
+import { styles } from '../styles';
+import { fadeIn, textVariant } from '../utils/motion';
+import { SectionWrapper } from '../hoc';
+
 const About = () => {
-  <section className="relative w-full hscreen mx-auto">
-    About
-  </section>
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <h2 className={styles.sectionHeadText}>About</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn('', '', 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem repellendus beatae provident, ea deserunt adipisci
+        a rerum voluptatibus dolor nulla sequi quae nesciunt pariatur illo error sint nisi amet dicta?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim veniam omnis quam labore deserunt nisi
+        accusantium, nam porro ipsum, at non. Voluptatum ex eum in, dolores hic odio atque ea.
+      </motion.p>
+    </>
+  )
 }
 
-export default About
+export default SectionWrapper(About, "about")
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,25 @@
+import { motion } from 'framer-motion';
+
+import { styles } from '../styles';
+
 const Hero = () => {
-  <section className="relative w-full hscreen mx-auto">
-    Hero
-  </section>
+  return (
+    <section className="relative w-full h-screen mx-auto">
+      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
+        <div className="flex flex-col justify-center items-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+        </div>
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>Welcome, <span className="text-[#915eff]">This is FCCBC</span></h1>
+          <br />
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            Crypto Investment Platform
+          </p>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default Hero
